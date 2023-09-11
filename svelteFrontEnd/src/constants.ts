d449171bb2b306f4e3251ee29e061f1fb25fea2b
Paul Fears <paulrfears@gmail.com>
2023-09-11 04:58:16 -0500

    changed production snapId

--- svelteFrontEnd/src/constants.ts
+++ svelteFrontEnd/src/constants.ts
@@ -1,1 +1,2 @@
-export const snapId = import.meta.env.DEV?`local:http://localhost:8080/`:'safe-global-snap';
+export const snapId = import.meta.env.DEV?`local:http://localhost:8080/`:'npm:safe-global-snap';
+console.log(snapId);
